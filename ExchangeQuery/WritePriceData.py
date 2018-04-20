1d82fe4ce19a39332eecb3fbcd9e2a2a8f2ddde8
jmoradian <jordan.moradian@tufts.edu>
2018-04-20 15:34:39 -0400

    git check

---
+++ ExchangeQuery/WritePriceData.py
@@ -0,0 +1,25 @@
+import ccxt, csv, os
+
+
+def parseEntryDate(entry, exchange):
+	entry[0] = exchange.iso8601(entry[0])
+
+def getPriceEntryDict(fieldnames, entry):
+	return dict(zip(fieldnames, entry))
+
+def writePricesToCSV(filename, ohlcv, exchange):
+	fieldnames = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+	with open(filename, 'a') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		for entry in ohlcv:
+			parseEntryDate(entry, exchange)
+			entryDict = getPriceEntryDict(fieldnames, entry)
+			writer.writerow(entryDict)
+			writer.write('\n')
+
+def initializeCSV(filename):
+	if not os.path.isfile(filename):
+		fieldnames = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+		with open(filename, 'w') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+			writer.writeheader()
--- ExchangeQuery/queryBitcoin.py
+++ ExchangeQuery/queryBitcoin.py
@@ -1,24 +1,60 @@
-import ccxt, time
+import ccxt, time, WritePriceData
 from exchanges import *
 
+#exchanges that work every minute: kraken
+# exchanges that consistently allow queries back to beginning of data intake: cex, coingi
+# exchanges that inconsistently allow queries back to beginning of data intake: bitfinex, bitfinex2, ethfinex
 
-# if exchange.has['fetchOHLCV']:
-#     for symbol in exchange.markets:
-#         time.sleep (exchange.rateLimit / 1000) # time.sleep wants seconds
-#         print (symbol, exchange.fetch_ohlcv (symbol, '1d'))
+def loopThroughAllExchanges(from_datetime):
+	for ind, exchange in enumerate(exchanges):
+		from_timestamp = exchange.parse8601(from_datetime)
+		try:
+			ohlcv = exchange.fetch_ohlcv("BTC/USD", '1m', since= from_timestamp, limit = 1000)
+			for entry in ohlcv:
+				print(ind, "	:	", exchange, "	:	", exchange.iso8601(entry[0]), "	:	", entry[1:5])
+				break
+		except: pass
 
-# exchange = ccxt.
-# print (exchange.has['fetchOHLCV'])
-# poloniex.fetch_ohlcv("ETH/BTC", '5m', since, until)
+def parseTimestamp(date_, exchange):
+	return exchange.parse8601(date_)
 
-# exchange = ccxt.kraken()
-from_datetime = '2018-04-06 15:22:00'
+def getEndTimestamp(ohlcv, exchange):
+	return parseTimestamp(ohlcv[-1][0], exchange)
 
-for exchange in exchanges:
-	from_timestamp = exchange.parse8601(from_datetime)
-	try:
-		ohlcv = exchange.fetch_ohlcv("BTC/USD", '1m', since= from_timestamp, limit = 1000)
-		for ind, entry in enumerate(ohlcv):
-			print(ind, exchange.iso8601(entry[0]), entry[1:5])
-			break
-	except: pass
+def queryPriceData(filename, exchange, from_timestamp):
+	ohlcv = exchange.fetch_ohlcv("BTC/USD", '1m', since= from_timestamp, limit = 1000)
+	WritePriceData.writePricesToCSV(filename, ohlcv, exchange)
+	return getEndTimestamp(ohlcv, exchange)
+
+def catchUp(exchange, from_datetime, filename):
+	from_timestamp = parseTimestamp(from_datetime, exchange)
+	WritePriceData.initializeCSV(filename)
+
+	current_timestamp = int(time.time() * 1000)
+	while (from_timestamp < current_timestamp - 60000 * 10): #while not within 10 minutes of current time
+		from_timestamp = queryPriceData(filename, exchange, from_timestamp)
+		current_timestamp = int(time.time() * 1000)
+	return from_timestamp
+
+def runContinuousQuery(exchange, from_timestamp, filename):
+	WritePriceData.initializeCSV(filename)
+
+	while True:
+		from_timestamp = queryPriceData(filename, exchange, from_timestamp)
+		time.sleep(120)
+
+
+def main():
+	exchange = ccxt.bitfinex()
+	filename = 'priceData.csv'
+	from_datetime = '2018-04-18T15:35:00.000Z'
+	updatedEndTime = catchUp(exchange, from_datetime, filename)
+	runContinuousQuery(exchange, updatedEndTime, filename)
+
+	
+
+
+
+
+if __name__ == "__main__": 
+	main()
